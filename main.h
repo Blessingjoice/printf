69e60aba5632c4c84fd2a37e38fcd42fac164185
Blessingjoice <blessingnwabuoku@gmail.com>
2023-09-16 06:57:58 -0700

    waiting on my partner

--- main.h
+++ main.h
@@ -18,7 +18,7 @@ void print_binary(unsigned int n, unsigned int *printed);
 int print_UintToBinary(va_list arg);
 int p_oct(va_list arg);
 int print_UIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
+int printBase(va_list arg, char _case);
 int pHexad(va_list arg);
 int pHex(va_list arg);
 int print_str(va_list arg);
---
+++ printhex.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <unistd.h>
+#include <math.h>
+/**
+ * printBase - base function for numbers
+ * @arg: argument
+ * @_case: (upper or lower)
+ * Return: number of digits printed
+ */
+
+int printBase(va_list arg, char _case)
+{
+	unsigned int num = va_arg(arg, unsigned int);
+	int digChars;
+
+	if (num == 0)
+		return (_putchar('0'));
+	digChars = print_UIntToHex(num, _case);
+
+	return (nbrCharacters);
+}
+/**
+ * pHexad - prints a hexadecimal in lower case
+ * @arg: list
+ * Return: number of digits printed
+ */
+
+int pHexad(va_list arg)
+{
+	return (printBase(arg, 'a'));
+}
+
+/**
+ * pHex - prints a hexadecimal in upper case
+ * @arg: list
+ * Return: number of digits printed
+ */
+
+int pHex(va_list arg)
+{
+	return (printBase(arg, 'A'));
+}
